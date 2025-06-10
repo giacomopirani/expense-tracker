694605efad352280a80c15e368b1d222962f2f76
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-06-10 19:24:15 +0200

    create context & add base of App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import GlobalState from "./context/index.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>
+  <GlobalState>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </GlobalState>
 );
