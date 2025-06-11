cae1b7ac6e87acdf7744998278bd83151b49bf6e
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-06-11 11:36:20 +0200

    create summary & expense-view component

--- src/assets/components/main/index.jsx
+++ src/assets/components/main/index.jsx
@@ -1,12 +1,44 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import ExpenseView from "../expense-view";
+import Summary from "../summary";
 
 export default function Main() {
   return (
-    <Flex
-      textAlign={"center"}
-      flexDirection={"column"}
-      pr={"5"}
-      pl={"5"}
-    ></Flex>
+    <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
+        <Heading
+          color={"blue.400"}
+          display={["none", "block", "block", "block", "block"]}
+          fontWeight={"extrabold"}
+        >
+          Expense Tracker
+        </Heading>
+        <Flex alignItems={"center"}>
+          <Button
+            bg={"blue.400"}
+            color={"white"}
+            ml={"4"}
+            _hover={{
+              bg: "blue.600",
+              transform: "scale(1.05)",
+              transition: "all 0.2s ease-in-out",
+            }}
+          >
+            Add New Transaction
+          </Button>
+        </Flex>
+      </Flex>
+      <Summary />
+
+      <Flex
+        w="full"
+        alignItems={"flex-start"}
+        justifyContent={"space-evenly"}
+        flexDirection={["column", "column", "column", "row", "row"]}
+      >
+        <ExpenseView />
+        <ExpenseView />
+      </Flex>
+    </Flex>
   );
 }
