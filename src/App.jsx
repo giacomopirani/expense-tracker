b414875ea6ec8d6188418bfb1e8c6c4ed9dce0fa
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-06-11 11:24:47 +0200

    start to create a dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Box, Container, Flex } from "@chakra-ui/react";
 import "./App.css";
+import Main from "./assets/components/main";
 
 function App() {
   return (
     <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
       <Flex height={"full"}>
-        <Box></Box>
+        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+          <Main />
+        </Box>
       </Flex>
     </Container>
   );
---
+++ src/assets/components/main/index.jsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Main() {
+  return (
+    <Flex
+      textAlign={"center"}
+      flexDirection={"column"}
+      pr={"5"}
+      pl={"5"}
+    ></Flex>
+  );
+}
