96e962088059f6b39033b7da8d05e4c2ec1ee292
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-09-20 14:30:33 +0200

    incomeController: create getAllIncome function for get all income present on DB

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -29,7 +29,16 @@ exports.addIncome = async (req, res) => {
 };
 
 // Get All Income User
-exports.getAllIncome = async (req, res) => {};
+exports.getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 // Add Income User
 exports.deleteIncome = async (req, res) => {};
