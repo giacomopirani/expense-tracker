1589c0ad2d72b00afac8e13dbc8713d7e0e53ec6
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-09-22 10:37:12 +0200

    frontend:(navbar): start to create navbar

--- frontend/expense-tracker/src/components/layouts/navbar.jsx
+++ frontend/expense-tracker/src/components/layouts/navbar.jsx
@@ -1,5 +1,34 @@
-const Navbar = () => {
-  return <div className="text-black">Navbar</div>;
+import { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
+import SideMenu from "./sidemenu";
+
+const Navbar = ({ activeMenu }) => {
+  const [opendSideMenu, setOpendSideMenu] = useState(false);
+
+  return (
+    <div className="flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
+      <button
+        className="block lg:hidden text-black"
+        onClick={() => {
+          setOpendSideMenu(!opendSideMenu);
+        }}
+      >
+        {opendSideMenu ? (
+          <HiOutlineX className="text-2xl" />
+        ) : (
+          <HiOutlineMenu className="text-2xl" />
+        )}
+      </button>
+
+      <h2 className="text-lg font-medium text-black ">Expense Tracker</h2>
+
+      {opendSideMenu && (
+        <div className="fixed top-[61px] -ml-4 bg-white">
+          <SideMenu activeMenu={activeMenu} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
