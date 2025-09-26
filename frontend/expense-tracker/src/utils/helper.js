7f59fa4c5d3047ce183a80cc49e6a63f756a19cd
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-09-26 11:29:10 +0200

    frontend:(helper): fix correct formatted amount balance

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -22,11 +22,8 @@ export const addThousandsSeparetor = (num) => {
   if (num === null || isNaN(num)) return "";
 
   const [integerPart, fractionalPart] = num.toString().split(".");
-  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-  return fractionalPart
-    ? `${formattedInteger}.${fractionalPart}`
-    : formattedInteger;
+  return fractionalPart ? `${integerPart}.${fractionalPart}` : integerPart;
 };
 
 export const prepareExpenseBarChartData = (data = []) => {
