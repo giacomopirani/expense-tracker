24c3a7711ffe24d97a5c4e265fd9c886c1c2eb56
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-10-01 15:44:50 +0200

    frontend:(transactionInfoCard): fix trash in mobile

--- frontend/expense-tracker/src/components/cards/transactionInfoCard.jsx
+++ frontend/expense-tracker/src/components/cards/transactionInfoCard.jsx
@@ -1,6 +1,5 @@
-import { LuTrash2, LuTrendingDown, LuTrendingUp } from "react-icons/lu";
-
 import { GiMoneyStack } from "react-icons/gi";
+import { LuTrash2, LuTrendingDown, LuTrendingUp } from "react-icons/lu";
 
 const TransactionInfoCard = ({
   title,
@@ -13,9 +12,10 @@ const TransactionInfoCard = ({
 }) => {
   const getAmountStyles = () =>
     type === "income" ? "bg-green-50 text-green-500" : "bg-red-50 text-red-500";
+
   return (
-    <div className="group relative flex items-center gap-4 mt-2 p-3 rounded-lg hover:bg-stone-200/50">
-      <div className="w-12 h-12 flex items-center justify-center texl-xl text-stone-800 bg-stone-100 rounded-full">
+    <div className="group relative grid grid-cols-[auto_1fr_auto] items-center gap-4 mt-2 p-3 rounded-lg hover:bg-stone-200/50">
+      <div className="w-12 h-12 flex items-center justify-center bg-stone-100 rounded-full text-stone-800">
         {icon ? (
           <img src={icon} alt={title} className="w-6 h-6" />
         ) : (
@@ -23,35 +23,33 @@ const TransactionInfoCard = ({
         )}
       </div>
 
-      <div className="flex-1 flex items-center justify-between">
-        <div>
-          <p className="texh-sm text-stone-700 font-medium">{title}</p>
-          <p className="text-xs text-stone-400 mt-1">{date}</p>
+      <div>
+        <p className="text-sm text-stone-700 font-medium">{title}</p>
+        <p className="text-xs text-stone-400 mt-1">{date}</p>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <div
+          className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}
+        >
+          <h6 className="text-xs font-medium">
+            {type === "income" ? "+" : "-"} €{amount}
+          </h6>
+          {type === "income" ? <LuTrendingUp /> : <LuTrendingDown />}
         </div>
 
-        <div className="flex items-center gap-2">
-          {!hideDeleteBtn && (
-            <button
-              className="
-    text-stone-400 hover:text-red-500
-    opacity-100 md:opacity-0 md:group-hover:opacity-100
-    transition-opacity cursor-pointer
-  "
-              onClick={onDelete}
-            >
-              <LuTrash2 size={18} />
-            </button>
-          )}
-
-          <div
-            className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}
+        {!hideDeleteBtn && (
+          <button
+            className="
+        text-stone-400 hover:text-red-500
+        opacity-100 md:opacity-0 md:group-hover:opacity-100
+        transition-opacity cursor-pointer
+      "
+            onClick={onDelete}
           >
-            <h6 className="text-xs font-medium">
-              {type === "income" ? "+" : "-"} €{amount}
-            </h6>
-            {type === "income" ? <LuTrendingUp /> : <LuTrendingDown />}
-          </div>
-        </div>
+            <LuTrash2 size={18} />
+          </button>
+        )}
       </div>
     </div>
   );
