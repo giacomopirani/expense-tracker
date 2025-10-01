700f4e0456e4c028596a92f746b8d2b136da4b80
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-10-01 15:51:29 +0200

    frontend:(transactionInfoCard): fix trash for mobile size

--- frontend/expense-tracker/src/components/cards/transactionInfoCard.jsx
+++ frontend/expense-tracker/src/components/cards/transactionInfoCard.jsx
@@ -30,19 +30,6 @@ const TransactionInfoCard = ({
         </div>
 
         <div className="flex items-center gap-2">
-          {!hideDeleteBtn && (
-            <button
-              className="
-    text-stone-400 hover:text-red-500
-    opacity-100 md:opacity-0 md:group-hover:opacity-100
-    transition-opacity cursor-pointer
-  "
-              onClick={onDelete}
-            >
-              <LuTrash2 size={18} />
-            </button>
-          )}
-
           <div
             className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}
           >
@@ -51,6 +38,19 @@ const TransactionInfoCard = ({
             </h6>
             {type === "income" ? <LuTrendingUp /> : <LuTrendingDown />}
           </div>
+
+          {!hideDeleteBtn && (
+            <button
+              className="
+        text-stone-400 hover:text-red-500
+        opacity-100 md:opacity-0 md:group-hover:opacity-100
+        transition-opacity cursor-pointer
+      "
+              onClick={onDelete}
+            >
+              <LuTrash2 size={18} />
+            </button>
+          )}
         </div>
       </div>
     </div>
